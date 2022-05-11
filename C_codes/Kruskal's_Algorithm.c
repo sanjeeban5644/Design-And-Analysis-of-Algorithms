02a9c763a6330a2d08a283fd88b35b585a97dea8
Sanjeeban <sanjeeban5644@gmail.com>
2022-05-11 21:19:57 +0530

    Major Update 3

--- C_codes/Kruskal's_Algorithm.c
+++ C_codes/Kruskal's_Algorithm.c
@@ -1,7 +1,10 @@
-
 #include<stdio.h>
-
 #define INFY 9999
+int arr[20];
+
+int uni(int, int );
+int find(int );
+
 
 int main(){
 	int vertex,edges,i,j,k,a,b,count=1,min,totcost=0,u,v,q,w,t;
@@ -27,37 +30,40 @@ int main(){
 		 }
 		 printf("\n");
 	 }
-	
-	int visited[vertex+1];
-	for(i=1;i<=vertex;i++){
-		visited[i]=0;
-	}
-	printf("\nEnter the starting vertex : ");
-	int start;
-	scanf("%d",&start);
-	visited[start]=1;
-	
-	while(count<vertex){
-		for(i=1,min=INFY;i<=vertex;i++){
-			for(j=1;j<=vertex;j++){
-				if(mat[i][j]<min){
-					if(visited[i]!=0){
-						min = mat[i][j];
-						a=u=i;
-						b=v=j;
-					}
-					if(visited[u]==0 || visited[v]==0){
-						printf("\nEdge %d : %d->%d = %d",count++,a,b,min);
-						totcost+=min;
-						visited[b]=1;
-					}
-				}
-			}
-			mat[a][b]=mat[b][a]=INFY;
-		}
-	}	
-	
+	 
+	 while(count<vertex){
+	 	for(i=1,min=INFY;i<=vertex;i++){
+	 		for(j=1;j<=vertex;j++){
+	 			if(mat[i][j]<min){
+	 				min=mat[i][j];
+	 				a=u=i;
+	 				b=v=j;
+				 }
+			 }
+		 }
+		 u=find(u);
+		 v=find(v);
+		 if(uni(u,v)){
+		 	printf("\nEdge %d : %d -> %d  =  %d",count++,a,b,min);
+		 	totcost+=min;
+		 }
+		 mat[a][b]=mat[b][a]=INFY;
+	 }
+	 
 	printf("\nTotal cost is : %d",totcost);
+}
+
+int find(int i){
+	while(arr[i]){
+		i=arr[i];
+	}
+	return i;
+}
+
+int uni(int i,int j){
+	if(i!=j){
+		arr[j]=i;
+		return 1;
+	}
 	return 0;
-	
 }
